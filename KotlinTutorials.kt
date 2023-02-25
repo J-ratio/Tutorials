cbfb70b5323f368b6412f40979f585539e93caae
r_@tio <f20201505@goa.bits-pilani.ac.in>
2023-02-25 14:21:31 +0530

    Added OOP starting summary

--- KotlinTutorials.kt
+++ KotlinTutorials.kt
@@ -32,7 +32,22 @@
     OOP: Delegation
     */
 
+    //-------------------------------------
 
+
+    /*OOP summary
+    Why does a paradigm like OOP work better when we already have a funtional way of coding?
+    -> While one would indeed see coding as a function based activity where all coded statements revolves around logic of the
+       problem statement, it fails to work with human understanding of the world we live in and the data produced by it.
+       Maybe in future, when all code is AI generated we will see a huge migration towards new codebases which are biased towards 
+       declarative and parallel way of programing. 
+
+    
+
+    //OOP: Classes (general format of comaprable entities within a program 
+                    ForEg: One can create a NPC class containg health variable
+                    and assign it to all NPCs in the game. As a result, all NPCs in the game now have a health component) 
+    
                     
 
 
