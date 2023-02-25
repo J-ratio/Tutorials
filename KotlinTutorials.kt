b49980835aecf8aa64798912b5c34c60a7c44b6b
r_@tio <f20201505@goa.bits-pilani.ac.in>
2023-02-25 14:21:01 +0530

    Added OOP topics index

--- KotlinTutorials.kt
+++ KotlinTutorials.kt
@@ -6,6 +6,32 @@
     //Kotlin Basics
 
 
+    //-------------------------------------------------------------------------------------------------------------------------------
+    /*OOP Concepts in Kotlin
+    OOP: Classes
+    OOP: Primary Constructor
+    OOP: Initializer Blocks
+    OOP: Secondary Constructors
+    OOP: Constructor Parameters Default Values
+    OOP: Getters and Setters
+    OOP: lateinit keyword
+    OOP: Companion Object
+    OOP: Singleton
+    OOP: Lazy Initialization
+    OOP: Enum Classes
+    OOP: Inner Classes
+    OOP Challenge
+    OOP: Inheritance 1
+    OOP: Inheritance 2
+    OOP: Sealed Class
+    OOP: Abstract Classes
+    OOP: Data Class
+    OOP: Interfaces 1
+    OOP: Interfaces 2
+    OOP: Object Expression
+    OOP: Delegation
+    */
+
 
                     
 
