895e885a28d151a3719ef75c5357e0592e91302a
r_@tio <f20201505@goa.bits-pilani.ac.in>
2023-03-03 00:30:47 +0530

    DiceRoll App

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    fun logging() {
+        Log.v(TAG, "Hello WOrld!")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollBtn = findViewById<Button>(R.id.button)
+
+        rollBtn.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            rollDice()
+            logging()
+        }
+    }
+
+    private fun rollDice() {
+
+        val dice = Dice(6)
+        findViewById<TextView>(R.id.textView).text = dice.roll().toString()
+    }
+
+    class Dice(private val sideNum: Int) {
+
+        fun roll(): Int {
+            return (1..sideNum).random()
+        }
     }
 }
+
--- MyApplication/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ MyApplication/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.myapplication
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,11 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun rollsDice() {
+        val dice = MainActivity.Dice(6)
+        assertTrue("NopeDoesn'tWork", dice.roll() in 1..6)
     }
 }
