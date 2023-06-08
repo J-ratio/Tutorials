9c8ffd28fa2772e287d80dcf9b1bf5506c4689ad
r_@tio <f20201505@goa.bits-pilani.ac.in>
2023-06-08 05:48:00 +0530

    VarsInC

---
+++ C Programming/2) Int_Float_And_Chars/PrimitiveTypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int Integer = 5;
+    float Float = 5.0344;
+    char Char[] = "char";
+
+    printf("Integer: %d, Float: %f, Char: %s", Integer,Float,Char);
+
+    return 0;
+}
+
+
